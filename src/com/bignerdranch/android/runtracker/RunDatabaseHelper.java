efdeae8f9f3b24c6016d7708625f0d1e3654e898
Matthew Farley <matfarley@gmail.com>
2014-02-14 10:45:36 +1300

    As at 14 Feb

--- src/com/bignerdranch/android/runtracker/RunDatabaseHelper.java
+++ src/com/bignerdranch/android/runtracker/RunDatabaseHelper.java
@@ -3,6 +3,7 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.location.Location;
 
 
 
@@ -12,6 +13,14 @@ public class RunDatabaseHelper extends SQLiteOpenHelper {
 	
 	private static final String TABLE_RUN = "run";
 	private static final String COLUMN_RUN_START_DATE = "start_date";
+	
+	private static final String TABLE_LOCATION = "location";
+	private static final String COLUMN_LOCATION_LATITTUDE = "latitude";
+	private static final String COLUMN_LOCATION_LONGITUDE = "longitude";
+	private static final String COLUMN_LOCATION_ALTITUDE = "altitude";
+	private static final String COLUMN_LOCATION_TIMESTAMP = "timestamp";
+	private static final String COLUMN_LOCATION_PROVIDER = "provider";
+	private static final String COLUMN_LOCATION_RUN_ID = "run_id";
 	
 	public RunDatabaseHelper(Context context){
 		super(context, DB_NAME, null, VERSION);
@@ -36,7 +45,18 @@ public class RunDatabaseHelper extends SQLiteOpenHelper {
 	public long insertRun(Run run){
 		ContentValues cv = new ContentValues();
 		cv.put(COLUMN_RUN_START_DATE, run.getStartDate().getTime()); //Key for the map should be the column name
-		return getWritableDatabase().insert(TABLE_RUN, null, cv);
+		return getWritableDatabase().insert(TABLE_RUN, null, cv); //Returns the run id;
+	}
+	
+	public long insertLocation(long runId, Location location){
+		ContentValues cv = new ContentValues();
+		cv.put(COLUMN_LOCATION_LATITTUDE, location.getLatitude());
+		cv.put(COLUMN_LOCATION_LONGITUDE, location.getLongitude());
+		cv.put(COLUMN_LOCATION_ALTITUDE, location.getAltitude());
+		cv.put(COLUMN_LOCATION_TIMESTAMP, location.getTime());
+		cv.put(COLUMN_LOCATION_RUN_ID, location.getProvider());
+		cv.put(COLUMN_LOCATION_RUN_ID, runId);
+		return getWritableDatabase().insert(TABLE_LOCATION, null, cv);
 	}
 
 }
